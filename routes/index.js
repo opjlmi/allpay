297431666f71dd103636b20e2c0289fbaf30a0cb
Sean Chen <opjlmi@gmail.com>
2016-06-23 21:01:05 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', key: '' });
 });
+
+router.post('/submit', function(req, res, next){
+  request.post({
+    url: "https://payment-stage.allpay.com.tw/AioHelper/GenCheckMacValue",
+    form: req.body,
+  }, function(err, httpResponse, body){
+    res.render('index', {title: 'Express', key: body})
+  })
+})
 
 module.exports = router;
